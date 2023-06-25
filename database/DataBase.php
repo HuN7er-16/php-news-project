8a03f0ad0db68312cc24649bd3f9457d1678c28e
Amirali Hosseini <amiralihosseini216@yahoo.com>
2023-06-25 12:05:35 +0330

    crud sql methods part 2 2/2

--- database/DataBase.php
+++ database/DataBase.php
@@ -78,4 +78,58 @@ class Database{
 
     }
 
+    //update
+    public function update($tableName, $id, $fields, $values){
+
+        $sql = "UPDATE " . $tableName . "SET ";
+        foreach(array_combine($fields, $values) as $field => $value){
+
+            if($value){
+
+                $sql .= "`" . $field . "` = ? ,";
+                
+            }else{
+                
+                $sql .= "`" . $field . "` = NULL ,";
+
+            }
+
+        }
+
+        $sql .= " updated_at = now()";
+        $sql .= " WHERE id = ?";
+        try{
+
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute(array_merge(array_filter(array_values($values)), [$id]));
+            return true;
+
+        }catch (PDOException $e) {
+            
+            echo $e->getMessage();
+            return false;
+
+        }
+
+    }
+
+    //delete
+    public function delete($tableName, $id){
+        
+        $sql = "DELETE FROM " . $tableName . " WHERE id = ? ;";
+        try{
+
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute([$id]);
+            return true;
+
+        }catch (PDOException $e) {
+            
+            echo $e->getMessage();
+            return false;
+
+        }
+
+    }
+
 }
