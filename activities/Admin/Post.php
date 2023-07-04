50e7089b7f9c8e1237c2f9cbd51bcbd5f44163c7
Amirali Hosseini <amiralihosseini216@yahoo.com>
2023-07-05 01:29:31 +0330

    edit of posts

--- activities/Admin/Post.php
+++ activities/Admin/Post.php
@@ -52,18 +52,42 @@ class Post extends Admin{
     }
 
     public function edit($id){
-
+    
         $db = new DataBase();
-        $Post = $db->select('SELECT * FROM categories WHERE `id` = ?;', [$id])->fetch();
+        $post = $db->select('SELECT * FROM posts WHERE `id` = ?;', [$id])->fetch();
+        $categories = $db->select('SELECT * FROM categories ORDER BY `id` ASC');
         require_once (BASE_PATH . '/template/admin/posts/edit.php');
 
     }
 
     public function update($request, $id){
 
+        $realTimestamp = substr($request['published_at'], 0, 10);
+        $request['published_at'] = date("Y-m-d H:i:s", (int)$realTimestamp);
         $db = new DataBase();
-        $db->update('categories', $id, array_keys($request), $request);
-        $this->redirect('admin/post');
+        if($request['cat_id'] != null){
+
+            if($request['image']['tmp_name'] != null){
+
+                $post = $db->select('SELECT * FROM posts WHERE `id` = ?;', [$id])->fetch();
+                $this->removeImage($post['image']);
+                $request['image'] = $this->saveImage($request['image'], 'post-image');
+
+            }else{
+                
+                unset($request['image']);
+
+            }
+            $request = array_merge($request, ['user_id' => 1]);
+            $db->update('posts', $id, array_keys($request), $request);
+            $this->redirect('admin/post');
+
+        }else{
+            
+            $this->redirect('admin/post');
+
+        }
+
 
     }
 
